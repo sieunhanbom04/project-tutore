eb5f01142fa2c2e22e95a53a6e603ac6b02ba332
Waiss Azizian <wazizian@clipper.ens.fr>
2017-09-09 12:16:46 +0200

    Added File

---
+++ problem.py
@@ -0,0 +1,66 @@
+from random import randint
+import matplotlib
+import matplotlib.pyplot as plt
+
+def generate(k,h, returnBits = False) :
+    l=[]
+    for i in range(h) :
+        x=randint(0,k)
+        while x in l :
+            x=randint(0,k)
+        l.append(x)
+    sum=0
+    for i in l :
+        sum+= 2**i
+    if not returnBits:
+        return sum
+    else:
+        return l, sum
+    
+def generate_problem(k,h) :
+    A=generate(k,h)
+    B=generate(k,h)
+    X=generate(k,h)
+    Y=generate(k,h)
+    C=(A*X+B*Y)%(2**k-1)
+    return A,B,C,X,Y
+    
+
+def hamming_weight(n) :
+    t=0
+    while n>0 :
+       t+=n%2
+       n//=2
+    return t
+    
+    
+def testPlot():
+    A,B,C,X,Y = generate_problem(40000,10)
+    k, h = 40000, 10
+    A=generate(k,h)
+    B=generate(k,h)
+    Xl, X=generate(k,h, returnBits = True)
+    Y=generate(k,h)
+    n = 2**k-1
+    C=(A*X+B*Y)%n
+    lst = [hamming_weight(C)]
+    for p in Xl:
+        C = (C -  2 ** p * A) % n
+        lst.append(hamming_weight(C))
+    plt.plot(list(range(h)), lst)
+    plt.draw()
+    
+def backtrack(A,B,C,h,k,n,result,p):
+    if p == k:
+        return False, None
+    elif len(result) == h:
+        return 
+    if hamming_weight(newC)==hamming_weight(C)-h:
+        newC=(C -  2 ** p * A) % n
+        
+        #backtrack(A,B
+        
+    
+     
+
+            
